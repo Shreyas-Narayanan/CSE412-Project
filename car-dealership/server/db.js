48d20fddd0596af94357f55b837b26c5b04fa949
ShreyasNarayanan18 <snaray52@asu.edu>
2021-11-29 14:15:34 -0700

    Added title for page

--- car-dealership/server/db.js
+++ car-dealership/server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "SHreyas",
+    password: "PASSWORD",
     host: "localhost",
     port: 5432,
     database: "cse412project"
